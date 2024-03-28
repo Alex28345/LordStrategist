e1fba21566c53cfca42831271e3f255de7877607
GruZbob <alexandre.bergeron@etu.univ-amu.fr>
2024-03-28 16:50:39 +0100

    cook header / footer finistre

--- App/styles.js
+++ App/styles.js
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         alignItems: 'center',
         justifyContent: 'space-around',
-        position: 'fixed',
+        position: 'absolute',
         bottom: 0,
         borderTopWidth: 3,
         borderTopColor: "#282828"
