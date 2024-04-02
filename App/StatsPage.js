504f42d535500579ee73fdfb60f08a0177e881ca
LeoG <103144119+LeoG1904@users.noreply.github.com>
2024-04-02 22:46:35 +0200

    bon si qlq a la foie vous pouvez tester de rajouter le graphe dans StatsPage.js

--- App/StatsPage.js
+++ App/StatsPage.js
@@ -5,10 +5,21 @@ import Header from "./Header";
 import ListPresenter from "./ListPresenter";
 import Footer from "./Footer";
 import {Provider} from "react-redux";
+import PlayerGraph from "./GrapheHP";
+
 
 const StatsPage = () =>{
+    const playerList = [
+        { id: 1, name: 'Gul\'dan', race: 'orc', hp: 50, mp: 200, role: 'dark sorcerer', guild: 'Hord' },
+        { id: 2, name: 'Grommash', race: 'orc', hp: 250, mp: 70, role: 'berserker', guild: 'Hord' },
+        { id: 3, name: 'Anduin', race: 'Human', hp: 70, mp: 200, role: 'Healer', guild: 'Alliance' },
+        { id: 4, name: 'Uther', race: 'Human', hp: 180, mp: 150, role: 'Tank Healer', guild: 'Alliance' }
+    ];
     return (
-        <Text>hey</Text>
+        <Text>J'en peu plus</Text>
+        /*<View>
+            <PlayerGraph playerList={playerList}/>
+        </View>*/
     )
 }
 
